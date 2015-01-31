6af4ebbdba3c1f25c2b8bbb6f32221a995641857
Tashakrn9 <Tashakrn9@TashKrn9>
2015-01-31 13:38:16 -0700

    This is the location for the project

--- bookAdventure/src/bookadventure/BookAdventure.java
+++ bookAdventure/src/bookadventure/BookAdventure.java
@@ -6,23 +6,34 @@
 package bookadventure;
 
 import bookadventure.model.Game;
+import bookadventure.model.Location;
 import bookadventure.model.Player;
+
+
 
 /**
  *
  * @author Echo
  */
 public class BookAdventure {
+    private static double xCoordinate;
+    private static double yCoordinate;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Location locationOne = new Location();
         Game gameOne = new Game();
         
+        locationOne.setxCoordinate(xCoordinate);
+        locationOne.setyCoordinate(yCoordinate);
+        locationOne.setVisited(null);
         gameOne.setTotalTime(25.00);
         gameOne.setAttribute("unknown");
         
+        String locationInfo = locationOne.toString();
+        System.out.println(locationInfo);
         String gameInfo = gameOne.toString();
         System.out.println(gameInfo);
     }
---
+++ bookAdventure/src/bookadventure/model/Location.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookadventure.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Tashakrn9
+ */
+public class Location implements Serializable {
+    private double xCoordinate;
+    private double yCoordinate;
+    private String visited;
+
+    public Location() {
+    }
+    
+    
+
+    public double getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(double xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public double getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(double yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public String getVisited() {
+        return visited;
+    }
+
+    public void setVisited(String visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "xCoordinate=" + xCoordinate + ", yCoordinate=" + yCoordinate + ", visited=" + visited + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.xCoordinate) ^ (Double.doubleToLongBits(this.xCoordinate) >>> 32));
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.yCoordinate) ^ (Double.doubleToLongBits(this.yCoordinate) >>> 32));
+        hash = 83 * hash + Objects.hashCode(this.visited);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (Double.doubleToLongBits(this.xCoordinate) != Double.doubleToLongBits(other.xCoordinate)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.yCoordinate) != Double.doubleToLongBits(other.yCoordinate)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+}
