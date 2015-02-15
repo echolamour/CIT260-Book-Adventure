7b2989a3e79e5175aa55918bf48286cd1a9b82fd
Tashakrn9 <Tashakrn9@TashKrn9>
2015-02-14 19:12:13 -0700

    this is the book adventure control test

---
+++ bookAdventure/test/bookadventure/control/BossControlTest.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookadventure.control;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tashakrn9
+ */
+public class BossControlTest {
+    
+    public BossControlTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of fightBoss method, of class BossControl.
+     */
+    @Test
+    public void testFightBoss() {
+        System.out.println("fightBoss");
+         /********************
+         * Test case #1
+         ********************/
+        System.out.println("\tTest case #1");
+        
+        //input values
+        double attack = 100.0;
+        long strength = 150;
+        double defense = 50.0;
+        
+        double expResult = 15.0;
+        BossControl instance = new BossControl();
+        
+        double result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 0000.0001);
+        
+        /********************
+         * Test case #2
+         ********************/
+        System.out.println("\tTest case #2");
+        
+        //input values
+         attack = 100.0;
+         strength = 150;
+         defense = -1.0;
+        
+         expResult = -1.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, -1.0);
+        
+             
+       
+        
+        /********************
+         * Test case #5
+         ********************/
+        System.out.println("\tTest case #5");
+        
+        //input values
+         attack = 50.0;
+         strength = 150;
+         defense = 700.0;
+        
+         expResult = -1.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 000.0001);
+        
+        /********************
+         * Test case #6
+         ********************/
+        System.out.println("\tTest case #6");
+        
+        //input values
+         attack = 75.0;
+         strength = 200;
+         defense = 50.0;
+        
+         expResult = 40.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 000.0001);
+        
+        /********************
+         * Test case #7
+         ********************/
+        System.out.println("\tTest case #7");
+        
+        //input values
+         attack = 100.0;
+         strength = 150;
+         defense = 50.0;
+        
+         expResult = 15.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 000.0001);
+        
+        /********************
+         * Test case #8
+         ********************/
+        System.out.println("\tTest case #8");
+        
+        //input values
+         attack = 100.0;
+         strength = 150;
+         defense = 0.0;
+        
+         expResult = -1.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 000.0001);
+        
+        /********************
+         * Test case #9
+         ********************/
+        System.out.println("\tTest case #9");
+        
+        //input values
+         attack = 100.0;
+         strength = 200;
+         defense = 60.0;
+        
+         expResult = 25.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 000.0001);
+        
+        /********************
+         * Test case #10
+         ********************/
+        System.out.println("\tTest case #10");
+        
+        //input values
+         attack = 100.0;
+         strength = 100;
+         defense = 50.0;
+        
+         expResult = 10.0;
+      
+        
+         result = instance.fightBoss(attack, defense, strength);
+        assertEquals(expResult, result, 000.0001);
+        
+    }
+    
+}
